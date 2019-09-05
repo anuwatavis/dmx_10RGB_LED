6245dce7213d44aee7046ada063015a3469a7cc1
anuwatavis <anuwat.avis@gmail.com>
2019-09-06 01:05:05 +0700

    set

--- intro.ino
+++ intro.ino
@@ -1,8 +1,22 @@
 #include "Tlc5940.h"
-#include ""
-void setup(){
-    
+#include <DMXSerial.h>
+void setup()
+{
+    Tlc.init();
+    DMXSerial.init(DMXReceiver);
 }
-void loop(){
-
+void loop()
+{   
+    //test led with Tlc5940
+    Tlc.clear();
+    Tlc.set(0, 4095);
+    Tlc.update();
+    //test all led with 1 Tlc
+    for(int i = 0 ; i<16 ; i++){
+        Tlc.set(i, 4095);
+    }
+    //test 2 tlc led 
+    for(int j = 16; j<32; j++){
+        Tlc.set(j, 4095);
+    }
 }
