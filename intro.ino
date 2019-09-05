9cb36ba4b449ffa866b57892bd764684820c0eb4
anuwatavis <anuwat.avis@gmail.com>
2019-09-06 01:11:18 +0700

    comment_version

--- intro.ino
+++ intro.ino
@@ -11,12 +11,13 @@ void loop()
     Tlc.clear();
     Tlc.set(0, 4095);
     Tlc.update();
-    //test all led with 1 Tlc
-    for(int i = 0 ; i<16 ; i++){
-        Tlc.set(i, 4095);
-    }
-    //test 2 tlc led 
-    for(int j = 16; j<32; j++){
-        Tlc.set(j, 4095);
-    }
+    // //test all led with 1 Tlc
+    // for(int i = 0 ; i<16 ; i++){
+    //     Tlc.set(i, 4095);
+    // }
+    // //test 2 tlc led 
+    // for(int j = 16; j<32; j++){
+    //     Tlc.set(j, 4095);
+    // }
+    
 }
